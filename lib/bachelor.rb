6c5f347bf2d3ce66e244ef38da7b6f373c17f3ec
Ignacio R Espino <irespino@capozios.com>
2018-09-10 03:13:10 +0000

    Done.

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,19 +1,72 @@
+require 'pry'
+
 def get_first_name_of_season_winner(data, season)
-  # code here
+  winner = ''
+  
+  data[season].each do |x|
+    if x["status"] == "Winner"
+      winner = x["name"]
+    end
+    
+  end
+  
+  winner = winner.split
+  winner[0]
+  
+end
+
+def contestant_data(data)
+data.values.flatten
 end
 
 def get_contestant_name(data, occupation)
-  # code here
+  contestants = contestant_data(data)
+  name = ''
+  contestants.each do |x|
+    if x["occupation"] == occupation
+      name = x["name"]
+    end
+  end
+  
+  name
+  
 end
 
 def count_contestants_by_hometown(data, hometown)
-  # code here
+  counter = 0
+  
+  contestants = contestant_data(data)
+  contestants.each do |x|
+    if x['hometown'] == hometown
+      counter += 1
+    end
+  end
+  
+  counter
+  
 end
 
 def get_occupation(data, hometown)
-  # code here
+  contestants = contestant_data(data)
+  array_con = []
+  contestants.each do |x|
+    if x["hometown"] == hometown
+      array_con << x['occupation']
+    end
+  end
+    
+    array_con.shift
 end
 
 def get_average_age_for_season(data, season)
-  # code here
+  counter = 0
+  added_ages = 0
+  
+  data[season].each do |x|
+    counter += 1
+    added_ages += x["age"].to_i
+  end
+  
+  (added_ages.to_f / counter.to_f).round
+  
 end
